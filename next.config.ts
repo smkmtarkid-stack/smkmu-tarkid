4169f3dddb35f0d8a81c6d700749c61533b0cdea
smkmu-tarkid <smkm.tarkid@gmail.com>
2026-09-04 09:47:34 +0700

    fix: whitelist supabase storage domain in next config for images

--- next.config.ts
+++ next.config.ts
@@ -11,6 +11,10 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "drive.google.com",
       },
+      {
+        protocol: "https",
+        hostname: "*.supabase.co",
+      },
     ],
   },
 };
